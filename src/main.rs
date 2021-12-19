42da047c95cfc04a109c5b84e3720c8f10a93256
TuftedDeer <tufteddeer@disroot.org>
2021-12-19 16:02:33 +0100

    code from basic_tcp_proxy crate

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod proxy;
+
+use crate::proxy::TcpProxy;
+
+fn main() {
+    let server = "127.0.0.1:4433";
+    log::info!("starting client");
+    let _proxy = TcpProxy::new(9091, server.parse().unwrap(), false);
+    loop {}
+}
---
+++ src/proxy.rs
@@ -0,0 +1,95 @@
+/**
+Based on the basic_ctp_proxy crate (https://crates.io/crates/basic_tcp_proxy), MIT/Apache-2.0
+https://github.com/jamesmcm/basic_tcp_proxy/blob/master/src/lib.rs
+*/
+
+use log::debug;
+use std::io::{BufRead, BufReader, Write};
+use std::net::SocketAddr;
+use std::net::{IpAddr, Ipv6Addr};
+use std::net::{TcpListener, TcpStream};
+
+/// TcpProxy runs one thread looping to accept new connections
+/// and then two separate threads per connection for writing to each end
+pub struct TcpProxy {
+    /// The handle for the outer thread, accepting new connections
+    pub forward_thread: std::thread::JoinHandle<()>,
+}
+
+impl TcpProxy {
+    /// Create a new TCP proxy, binding to listen_port and forwarding and receiving traffic from
+    /// proxy_to
+    pub fn new(
+        listen_port: u16,
+        proxy_to: SocketAddr,
+        local_only: bool,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        let ip = if local_only {
+            Ipv6Addr::LOCALHOST
+        } else {
+            Ipv6Addr::UNSPECIFIED
+        };
+        let listener_forward = TcpListener::bind(SocketAddr::new(IpAddr::V6(ip), listen_port))?;
+
+        let forward_thread = std::thread::spawn(move || {
+            loop {
+                let (stream_forward, _addr) = listener_forward
+                    .accept()
+                    .expect("Failed to accept connection");
+                debug!("New connection");
+
+                let mut sender_forward = TcpStream::connect(proxy_to).expect("Failed to bind");
+                let sender_backward = sender_forward.try_clone().expect("Failed to clone stream");
+                let mut stream_backward =
+                    stream_forward.try_clone().expect("Failed to clone stream");
+
+                std::thread::spawn(move || {
+                    let mut stream_forward = BufReader::new(stream_forward);
+                    loop {
+                        let length = {
+                            let buffer = stream_forward.fill_buf().unwrap();
+                            let length = buffer.len();
+                            if buffer.is_empty() {
+                                // Connection closed
+                                debug!("Client closed connection");
+                                return;
+                            }
+                            sender_forward
+                                .write_all(&buffer)
+                                .expect("Failed to write to remote");
+                            sender_forward.flush().expect("Failed to flush remote");
+                            length
+                        };
+                        stream_forward.consume(length);
+                    }
+                });
+
+                let _backward_thread = std::thread::spawn(move || {
+                    let mut sender_backward = BufReader::new(sender_backward);
+                    loop {
+                        let length = {
+                            let buffer = sender_backward.fill_buf().unwrap();
+                            let length = buffer.len();
+                            if buffer.is_empty() {
+                                // Connection closed
+                                debug!("Remote closed connection");
+                                return;
+                            }
+                            if stream_backward.write_all(&buffer).is_err() {
+                                // Connection closed
+                                debug!("Client closed connection");
+                                return;
+                            }
+
+                            stream_backward.flush().expect("Failed to flush locally");
+                            length
+                        };
+                        sender_backward.consume(length);
+                    }
+                });
+            }
+        });
+
+        Ok(Self { forward_thread })
+    }
+}

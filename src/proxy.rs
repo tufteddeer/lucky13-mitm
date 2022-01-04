bba63686944da9f13b42d70454b63c62ffb6beeb
TuftedDeer <tufteddeer@disroot.org>
2022-01-04 17:07:46 +0100

    use header.content_len to read the whole tls record into one buffer

--- src/proxy.rs
+++ src/proxy.rs
@@ -6,7 +6,7 @@ https://github.com/jamesmcm/basic_tcp_proxy/blob/master/src/lib.rs
 */
 
 use log::debug;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::SocketAddr;
 use std::net::{IpAddr, Ipv6Addr};
 use std::net::{TcpListener, TcpStream};
@@ -49,43 +49,56 @@ impl TcpProxy {
                 std::thread::spawn(move || {
                     let mut stream_forward = BufReader::new(stream_forward);
                     loop {
-                        let length = {
-                            let buffer = stream_forward.fill_buf().unwrap();
-                            let length = buffer.len();
-                            if buffer.is_empty() {
-                                // Connection closed
-                                debug!("Client closed connection");
-                                return;
-                            }
 
-                            let mut forward_buff = vec![0u8; length];
-                            forward_buff.copy_from_slice(buffer);
+                        let buffer = stream_forward.fill_buf().unwrap();
+
+                        if buffer.is_empty() {
+                            // Connection closed
+                            debug!("Client closed connection");
+                            return;
+                        }
+
+                        let mut length = buffer.len();
+                        let mut forward_buff = vec![0u8; length];
+                        forward_buff.copy_from_slice(buffer);
 
                         if length >= TLS_HEADER_SIZE {
 
-                                let header = read_header(&buffer);
-                                if header.version == TLS_V_1_2 && header.content_type == APPLICATION_CONTENT {
-                                    println!("found app content");
+                            println!("buff size is {}, reading header", buffer.len());
+                            let header = read_header(&buffer);
+                            if header.version == TLS_V_1_2 && header.content_type == APPLICATION_CONTENT {
+                                println!("found app content");
 
-                                    //forward_buff[length-1] = 0x01;
+                                println!("content size: {}", header.content_len);
+                                println!("buff size: {}", buffer.len());
+                                println!("buffer content: {:?}", buffer);
 
-                                    for i in 1 .. min(900, length - 4) {
-                                        forward_buff[length-i] =  0x11;
-                                        println!("{}", i)
-                                    }
-                                    /*
-                                    forward_buff[length-1] = 0x03;
-                                    forward_buff[length-2] = 0x03;
-                                    forward_buff[length-3] = 0x03;*/
-                                }
+                                // since there was no call to stream_forward.consume yet,
+                                // we can read TLS_HEADER_SIZE + content_len bytes (aka read the header again and wait for the rest of the record)
+
+                                // aaaaand it seems to work! (as long as stream_forward.fill_buf() gives us a buffer that contains the tls record right at the start)
+
+                                // content_len does not include the 5 tls header bytes
+                                let tls_record_size = TLS_HEADER_SIZE + header.content_len;
+
+                                let mut tls_record_buff = vec![0 as u8; tls_record_size];
+                                stream_forward.read_exact(&mut tls_record_buff).expect("failed to read whole tsl record with");
+                                println!("got record: {:02X?}", tls_record_buff);
+                                println!("record length: {:?}", tls_record_buff.len());
+
+                                length = tls_record_buff.len();
+                                // forward the whole tls record we captured
+                                forward_buff = tls_record_buff;
+
+                                // modify buffer
+                                //forward_buff[length-1] = 0x12;
                             }
+                        }
 
-                            sender_forward
-                                .write_all(&forward_buff)
-                                .expect("Failed to write to remote");
-                            sender_forward.flush().expect("Failed to flush remote");
-                            length
-                        };
+                        sender_forward
+                            .write_all(&forward_buff)
+                            .expect("Failed to write to remote"); // this will fail when the tls server terminates the connection after our manipulation/the alert is sent
+                        sender_forward.flush().expect("Failed to flush remote");
                         stream_forward.consume(length);
                     }
                 });
